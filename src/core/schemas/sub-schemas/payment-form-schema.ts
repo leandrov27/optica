17f18059d1715165a2c467a612d330d1f9ed02b1
leandrov27 <leoleandro2703@gmail.com>
2025-12-12 16:14:37 -0300

    change order

--- src/core/schemas/sub-schemas/payment-form-schema.ts
+++ src/core/schemas/sub-schemas/payment-form-schema.ts
@@ -6,7 +6,8 @@ import z from 'src/libs/zod';
 export const PAYMENT_FORM_OPTIONS = [
     { key: '01', label: 'Efectivo' },
     { key: '03', label: 'Transferencia' },
-    { key: '04', label: 'Tarjeta de Crédito' },
+    { key: '04', label: 'Transferencia NF' },
+    { key: '05', label: 'Tarjeta de Crédito' },
     { key: '28', label: 'Tarjeta de Débito' },
     { key: '99', label: 'Por Definir' },
 ] as const;
