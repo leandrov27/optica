494eed0083df658bbc3d55632568b20b7436dfee
leandrov27 <leoleandro2703@gmail.com>
2025-12-12 13:09:26 -0300

    payment form fix

--- src/core/schemas/sub-schemas/payment-form-schema.ts
+++ src/core/schemas/sub-schemas/payment-form-schema.ts
@@ -4,8 +4,8 @@ import z from 'src/libs/zod';
 // ----------------------------------------------------------------------
 
 export const PAYMENT_FORM_OPTIONS = [
-    { key: '01', label: 'Efectivo (Facturable)' },
-    { key: '03', label: 'Transferencia (Facturable)' },
+    { key: '01', label: 'Efectivo' },
+    { key: '03', label: 'Transferencia' },
     { key: '04', label: 'Tarjeta de Crédito' },
     { key: '28', label: 'Tarjeta de Débito' },
     { key: '99', label: 'Por Definir' },
