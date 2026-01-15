7ed79cf2eace08a65c9fe8f4ec41fe5de76e8411
leandrov27 <leoleandro2703@gmail.com>
2026-01-15 11:36:32 -0300

    fix prisma client

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,28 +1,29 @@
-/*import { PrismaClient } from 'src/generated/prisma/client';
-
-const globalForPrisma = globalThis as unknown as {
-  db: PrismaClient | undefined;
-};
-
-const db =
-  process.env.NODE_ENV === 'production'
-    ? new PrismaClient()
-    : globalForPrisma.db ?? (globalForPrisma.db = new PrismaClient());
-
-export default db;*/
-
 import 'dotenv/config';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import { PrismaClient } from 'src/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 10,
-});
+// ----------------------------------------------------------------------
 
-const db = new PrismaClient({ adapter });
+const createPrismaClient = () => {
+  const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5,
+  });
+
+  return new PrismaClient({ adapter });
+};
+
+// ----------------------------------------------------------------------
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
+
+const db = globalForPrisma.prisma ?? createPrismaClient();
 
 export default db;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
